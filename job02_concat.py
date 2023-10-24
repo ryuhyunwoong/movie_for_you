e132a006d890c1f527e8d12df0ac2868b70acb50
ryuhyunwoong <rrazx7@naver.com>
2023-10-24 11:39:49 +0900

    job04

---
+++ job02_concat.py
@@ -0,0 +1,16 @@
+import glob
+import pandas as pd
+
+data_paths = glob.glob('./crawling_data/*')
+print(data_paths)
+
+df = pd.DataFrame()
+for path in data_paths:
+    df_temp = pd.read_csv(path)
+    df_temp.dropna(inplace=True)
+    df_temp.drop_duplicates(inplace = True)
+    df = pd.concat([df, df_temp], ignore_index=True)
+df.drop_duplicates(inplace = True)
+df.info()
+my_year = 2023
+df.to_csv('./crawling_data/reviews_{}.csv'.format(my_year), index=False)
